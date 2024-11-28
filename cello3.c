e11f8177f4d8fb79acfb7061fd42c8037e28b579
Bill Morgan <arthurwilliammorgan@gmail.com>
2024-11-28 07:08:56 -0600

    build in parallel

---
+++ cello3.c
@@ -0,0 +1,8 @@
+#include "src/Show.c"
+#include "src/Start.c"
+#include "src/String.c"
+#include "src/Table.c"
+#include "src/Thread.c"
+#include "src/Tree.c"
+#include "src/Tuple.c"
+#include "src/Type.c"
